f17759d29bba1a2ee99bf26a0db87daa2af6ba97
DESKTOP-NFUG9C6\tjrgu <bshpublic062@gmail.com>
2023-01-20 13:18:22 +0900

    2023/01/20 백준문제풀이(반복문 합)

---
+++ BAEKJOON_03/C_Sharp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = 0;
+
+            for (int i = 1; i <= a; i++)
+            {
+                b += i;
+            }
+            Console.WriteLine(b);
+        }
+    }
+}
