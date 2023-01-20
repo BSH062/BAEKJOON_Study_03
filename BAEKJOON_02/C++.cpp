eda0ebd02f41a297f9c1069aafa95b605425335b
DESKTOP-NFUG9C6\tjrgu <bshpublic062@gmail.com>
2023-01-20 11:23:06 +0900

    2023/01/20 백준문제풀이  (반복문)

---
+++ BAEKJOON_02/C++.cpp
@@ -0,0 +1,14 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main()
+{
+	int line , a, b;
+	scanf("%d", &line);
+
+	for (int i = 0; i < line; i++)
+	{
+		scanf("%d %d", &a, &b);
+		printf("%d\n", a + b);
+	}
+}
---
+++ BAEKJOON_02/C_Sharp.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string line = Console.ReadLine();
+            //int Line = int.Parse(line);
+
+            //for (int i = 0; i < Line; i++)
+            //{
+            //    string[] input = Console.ReadLine().Split();
+            //    int[] rel = Array.ConvertAll(input, int.Parse);
+
+            //    Console.WriteLine(rel[0] + rel[1]);
+            //}
+
+
+            //아래가 좀더 문제에 원하는 답인것 같음 
+            int a = int.Parse(Console.ReadLine());
+            int[] arrA = new int[a];
+            int[] arrB = new int[a];
+
+            for (int i = 0; i < a; i++)
+            {
+                string[] s = Console.ReadLine().Split();
+                int[] rel = Array.ConvertAll(s, int.Parse);
+
+                arrA[i] = rel[0];
+                arrB[i] = rel[1];
+            }
+
+            for (int i = 0; i < a; i++)
+            {
+                Console.WriteLine(arrA[i] + arrB[i]);
+            }
+        }
+    }
+}
