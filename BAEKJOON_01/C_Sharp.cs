c51eb1cd788350e72f7008a9d8bfb22454be896c
DESKTOP-NFUG9C6\tjrgu <bshpublic062@gmail.com>
2023-01-20 10:16:39 +0900

    2023/01/20 백준문제풀이 (구구단)

---
+++ BAEKJOON_01/C_Sharp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int rel = int.Parse(input);
+
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine(rel+" * "+i+" = "+rel*i);
+            }
+        }
+    }
+}
