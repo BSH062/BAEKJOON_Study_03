ab1ad08d359a696c27b9e5167362f54079b12482
DESKTOP-NFUG9C6\tjrgu <bshpublic062@gmail.com>
2023-01-25 18:18:27 +0900

    2023/01/25 백준문제풀이(빠른 A+B)

---
+++ BAEKJOON_05/C++.cpp
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main()
+{
+	int line,a,b;
+	scanf("%d", &line);
+	//동적할당
+	int* rel = new int[line];
+		
+	for (int i = 0; i < line; i++)
+	{
+		scanf("%d %d", &a, &b);
+		rel[i] = a + b;
+	}
+	for (int i = 0; i < line; i++)
+	{
+		printf("%d\n", rel[i]);
+	}
+
+}
---
+++ BAEKJOON_05/C_Sharp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int line=int.Parse(Console.ReadLine());
+            int[] relA = new int[line];
+            int[] relB = new int[line];
+
+            var adnum = new StringBuilder();
+            for (int i = 0; i < line; i++)
+            {
+                string[] input = Console.ReadLine().Split( );
+                int[] rel = Array.ConvertAll(input, int.Parse);
+                relA[i] = rel[0];
+                relB[i] = rel[1];
+                adnum.Append(relA[i] + relB[i]+"\n");
+            }
+            Console.WriteLine(adnum.ToString());
+        }
+    }
+}
